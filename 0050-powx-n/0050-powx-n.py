cabafac96197db100bbfc5a71b0f36521242bb01
Gautam Kumar <75409870+Gautamcodes24@users.noreply.github.com>
2026-08-24 12:14:28 +0530

    Time: 0 ms (100%), Space: 19.6 MB (20.11%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,15 +1,23 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1.0
-        # Handle negative exponent
-        if n < 0:
-            x = 1 / x
-            n = -n
         ans = 1.0
-        while n > 0:
-            if n % 2 == 1:
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1/x
+            n *= -1
+        def calpow(x,pow):
+            nonlocal ans
+            if pow == 0:
+                return
+            if pow % 2 == 1:
                 ans *= x
-            x *= x
-            n //= 2
+            pow = pow // 2
+            return calpow(x * x , pow)
+        calpow(x,n)
         return ans
+
+            
+
+
+        
