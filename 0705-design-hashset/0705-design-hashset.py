02d5fd115b9f1072e5e08d14cf787e191955cfae
Gautam Kumar <75409870+Gautamcodes24@users.noreply.github.com>
2026-09-26 22:19:09 +0530

    Time: 53 ms (47.19%), Space: 46 MB (10.87%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -2,6 +2,7 @@ class MyHashSet:
 
     def __init__(self):
         self.hashset = [False] * 1000001
+
         
 
     def add(self, key: int) -> None:
@@ -10,7 +11,6 @@ class MyHashSet:
 
     def remove(self, key: int) -> None:
         self.hashset[key] = False
-
         
 
     def contains(self, key: int) -> bool:
